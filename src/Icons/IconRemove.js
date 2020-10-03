093131bc6473d6217518596620b5c4e611187e5b
Vinci Taylaran <svl.taylaran@gmail.com>
2020-10-03 12:13:16 -0600

    create Icons folder, create new CLI command that creates JSX components from the SVG files in assets

---
+++ src/Icons/IconRemove.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+function SvgIconRemove(props) {
+  return (
+    <svg width={14} height={14} {...props}>
+      <path
+        fill="#FFF"
+        fillRule="evenodd"
+        d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"
+      />
+    </svg>
+  );
+}
+
+export default SvgIconRemove;
