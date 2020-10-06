04053e6a0f3a35323aaad6f0049958c30617247d
Vinci Taylaran <svl.taylaran@gmail.com>
2020-10-06 12:51:24 -0600

    create criteria and selectedKeyword states for filtering listings, update script in useEffect, include filter method

--- src/App.tsx
+++ src/App.tsx
@@ -2,41 +2,57 @@ import React, { useState, useEffect } from "react"
 import Listing from "./components/Listing"
 import data from "./data.json"
 import { Props as IListing } from "./components/Listing"
-
-// <Header />
-// <Criteria />
-// data.map(listing => <Listing {...listing} />)
+import Criteria from "./components/Criteria"
 
 function App() {
   const [listings, setListings] = useState<IListing[]>([])
+  const [criteria, setCriteria] = useState<string[]>([])
+  const [selectedKeyword, setSelectedKeyword] = useState<string>()
 
   useEffect(() => {
-    function createListings(): IListing[] {
-      const listings: IListing[] = data.map((listing) => {
-        return {
-          ...listing,
-          logo: `../Icons/${listing.company}`,
-          isNew: listing.new, // TODO: find a way to override Object prototype "new" keyword
-          isFeatured: listing.featured,
-          keywords: [
-            listing.role,
-            listing.level,
-            ...listing.languages,
-            ...listing.tools,
-          ],
-        }
-      })
+    const listingsCopy = JSON.parse(JSON.stringify(data))
 
-      return listings
-    }
+    let listingsWithKeywords = listingsCopy.map((listing) => {
+      const keywords = [
+        listing.role,
+        listing.level,
+        ...listing.languages.map((language) => language),
+        ...listing.tools.map((tool) => tool),
+      ]
+      return { ...listing, keywords }
+    })
 
-    setListings(createListings())
+    setListings(listingsWithKeywords)
   }, [])
+
+  const filter = (keyword?: string) => {
+    console.log("Listings before filter -> ", listings)
+    if (keyword) {
+      const keywordLowercase = keyword.toLowerCase()
+      // I don't push keywordLowercase because I want the text in the components that will render the criteria array to have the original casing.
+      const criteriaCopy: string[] = [...criteria]
+      criteriaCopy.push(keyword)
+      setCriteria(criteriaCopy)
+
+      console.log("criteria -> ", criteria)
+
+      console.log(`keywordLowercase -> ${keywordLowercase}`)
+      setSelectedKeyword("keywordLowercase")
+      console.log(`selectedKeyword -> ${selectedKeyword}`)
+      setListings(
+        listings.filter((listing) => {
+          return listing.keywords.some((word) => word === selectedKeyword)
+        })
+      )
+      console.log("Listings after filter -> ", listings)
+    }
+  }
 
   return (
     <main>
+      <Criteria />
       {listings.map((listing) => (
-        <Listing key={listing.id} {...listing} />
+        <Listing {...listing} key={listing.id} onKeyword={filter} />
       ))}
     </main>
   )
