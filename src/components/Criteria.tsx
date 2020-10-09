a3233be04679577e37eee9cfea978462ed252cf6
Vinci Taylaran <svl.taylaran@gmail.com>
2020-10-09 17:43:26 -0600

    Update Criteria.tsx

--- src/components/Criteria.tsx
+++ src/components/Criteria.tsx
@@ -47,10 +47,10 @@ const StyledRemoveButton = styled.button`
     align-items: center;
   }
 
-  @media all and (max-width: ${BREAKPOINT.mobile}) {
+  /* @media all and (max-width: ${BREAKPOINT.mobile}) {
     width: 5px;
     padding-right: 25px;
-  }
+  } */
 
   &:hover {
     background-color: #2b3939;
